1ce35697cfae0f9a7bd237f746a8d37b33c760c2
Matt Brealey <matt@mattbrealey.com>
2020-10-04 15:52:34 +0100

    Implement LogTime

--- python/SFE/superFancyEncryption.py
+++ python/SFE/superFancyEncryption.py
@@ -4,6 +4,11 @@ v3 changelog:
     # updated weekly passphrase
 '''
 
+from datetime import datetime
+
+def getLogTime():
+    return datetime.now().strftime("%m/%d/%Y__%H:%M:%S")
+
 L = len
 R = range
 O = ord
@@ -16,32 +21,32 @@ X = "oh hello there you dapper dan"
 
 # PUBLIC API
 def encodeMessage(password, message): 
-    print("SFELOG.INFO: Encoding...")
+    print("SFELOG.INFO ({}): Encoding...".format(getLogTime()))
     try:
         return c(Y(password), message, 1)
     except:
-        print("SFELOG.ERROR: Could not encode message")
+        print("SFELOG.ERROR: Could not encode message".format(getLogTime()))
         return None
 
 def decodeMessage(password, message): 
-    print("SFELOG.INFO: Decoding...")
+    print("SFELOG.INFO ({}): Decoding...".format(getLogTime()))
     try:
         return c(Y(password), message, -1)
     except:
-        print("SFELOG.ERROR: Could not decode message")
+        print("SFELOG.ERROR ({}): Could not decode message".format(getLogTime()))
         return None
 
 # INTERNAL
 def Y(p):
-    print("SFELOG.DEBUG: Generating Y")
+    print("SFELOG.DEBUG ({}): Generating Y".format(getLogTime()))
     return [y for (x, y) in E(p) if x%2 == 0] if L(p)%2 else [y for (x, y) in E(p) if x%2 != 0]
 
 def c(p, m, d):
-    print("SFELOG.DEBUG: Generating C")
+    print("SFELOG.DEBUG ({}): Generating C".format(getLogTime()))
     return J([g(p[i % L(p)], m[i], L(p), i, d) for i in R(L(m))])
 
 def g(p, m, l, i, d):
-    print("SFELOG.DEBUG: Generating G")
+    print("SFELOG.DEBUG ({}): Generating G".format(getLogTime()))
     return C(((O(m) - O(p) + 256) % 256) - l*l*((i+1)%3) + L(X)) if d < 0 else chr((O(m) + O(p) % 256) + l*l*((i+1)%3) - L(X))
 
-print("SFELOG.INFO: SFE v0.2 loaded successfully.")
+print("SFELOG.INFO ({}): SFE v0.2 loaded successfully.".format(getLogTime()))
