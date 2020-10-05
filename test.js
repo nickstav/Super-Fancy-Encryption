0909f5f24a47a23b6344793fdecd7743867bc664
nickstav <nickjblackman@hotmail.com>
2020-10-05 20:32:02 +0100

    working test file

--- test.js
+++ test.js
@@ -11,43 +11,59 @@ TODOS:
 //Main function that calls the python process, and returns the cleaned, processed output of encode.py
 async function main() {
     let test = await runPythonSFE('nick', 'oh hello you silly goose', 'python/encode.py')
-    console.log(test);
+    console.log(JSON.parse(test));
 }
 
 //Function that creates the python process (passing in the script name + args), and adds pipes/onClose handling
 async function runPythonSFE(password, message, filePath) {
+    const { spawn } = require("child_process");
     console.log('Piping data from Python script ...');
 
-	let result = await new Promise(resolve => {
-        const { spawn } = require("child_process");
+    let result = [];
+
+	await new Promise((resolve, reject) => {
 
         // spawn new child process to call the python script using an absolute path
         const python = spawn('python3', [filePath, password, message]);
         
 		// collect data from script
-        python.stdout.on('data', resolve);
+        python.stdout.on('data', (data)=>{collectData(data, result)});
         
 		// the 'close' event is emitted when the stdio streams of a child process have been closed. 
-        python.on('close', confirmClosed);
+        python.on('close', (code)=>{confirmClosed(code, resolve, reject)});
         
 		// catch errors
 		python.stderr.on('data', handleError);
     });
-    
-    // the above returns a buffer object which we convert to a string
-    let bufferAsString = result.toString();
-    //get the JSON section of string by searching for the opening bracket & taking the string from there
-    let returnedData = "{" + bufferAsString.split('{')[1];
 
-    return JSON.parse(returnedData);
+    return result;
+}
+
+function collectData(data, variableToStore) {
+    let stream = data.toString();
+    variableToStore.push(filterData(stream));
+}
+
+function filterData(string) {
+    /* \r\n for Windows, \r for Mac, \n for Linux , then ignoring empty lines */
+    let lines = string.split(/\r\n|\r|\n/).filter(line => line !== ''); 
+    let codingOutput = lines.filter(line => !line.includes('SFELOG'));
+    
+    if (codingOutput.length === 1) {
+        return codingOutput[0];
+    } else {
+        return undefined;
+    }
 }
 
 //Function that runs when the python process is eventually closed
-function confirmClosed(code) {
+function confirmClosed(code, resolve, reject) {
     if (code === 0) {
         console.log(`Child process closed with code ${code}`);
+        resolve();
     } else {
-        console.log('Error: Process could not be completed');
+        console.log(`Error: Process could not be completed (${code})`);
+        reject();
     }
 }
 
